a8c886989ba67c7eb74055ab708b52970ddd625a
jbkunst <jbkunst@gmail.com>
2019-11-16 10:13:22 -0300

    ejercicio

--- apps/script-export.R
+++ apps/script-export.R
@@ -1,27 +1,26 @@
 if(!require(forecast)) install.packages("forecast")
 if(!require(xts)) install.packages("xts")
 if(!require(tradestatistic)) install.packages("tradestatistic")
+if(!require(ggplot2)) install.packages("ggplot2")
 
 library(forecast)
 library(xts)
 library(tradestatistics)
 library(dplyr)
-
-
-View(tradestatistics::ots_countries)
+library(ggplot2)
 
 pais <- "chn"
 
-yr <- ots_create_tidy_data(years = 1990:2018, reporters = pais, table = "yr")
-yr
+data <- ots_create_tidy_data(years = 1990:2018, reporters = pais, table = "yr")
 
-valores <- yr$export_value_usd
-fechas <- as.Date(paste0(yr$year, "0101"), format = "%Y%m%d",)
+valores <- data$export_value_usd
+fechas <- as.Date(paste0(data$year, "0101"), format = "%Y%m%d",)
 
 serie <- xts(valores, order.by = fechas)
-
-plot(serie)
-
 prediccion <- forecast(serie, h = 5)
 
-plot(prediccion)
+# IMPORTANTE: estas predicciones no *son las mas mejores* la idea  
+# del ejercicio es imaginar que tenemos un proceso el 
+# cual transformaremos en una app
+autoplot(prediccion)
+
