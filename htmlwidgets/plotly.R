2486dcb50cb574756db3bf8db5f2a2481e76c2bc
Joshua <jbkunst@gmail.com>
2020-11-07 12:58:50 -0300

    htmlwidgets

--- htmlwidgets/plotly.R
+++ htmlwidgets/plotly.R
@@ -4,6 +4,19 @@ if(!require(plotly)) install.packages("plotly")
 library(ggplot2)
 library(plotly)
 
-economics_long
-ggplot()
+data(economics_long)
 
+p <- ggplot(economics_long) +
+  geom_line(aes(x = date, y = value, group = variable, color = variable)) +
+  facet_wrap(vars(variable), scales = "free")
+
+p
+
+p +
+  theme_minimal() +
+  theme(legend.position = "bottom")
+  
+  
+plotly::ggplotly(p)
+  
+  
